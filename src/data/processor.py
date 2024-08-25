3467fcd18a02dc9ebffbc80ea2395293149f84bc
Satvik Jadhav <satvikj5@gmail.com>
2024-08-25 23:42:06 +0530

    adding a better way to filter for days and stoploss

--- src/data/processor.py
+++ src/data/processor.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from typing import List
+from typing import List, Optional
 
 
 class DataProcessor:
@@ -7,12 +7,19 @@ class DataProcessor:
     def filter_last_x_days(data: pd.DataFrame, x: int) -> pd.DataFrame:
         latest_date = data['Entry Date'].max()
         return data[data['Entry Date'] >= (latest_date - pd.Timedelta(days=x))]
-
-    @staticmethod
-    def exclude_days_or_stoploss(df: pd.DataFrame, exclude_days: List[str] = None, exclude_stoploss: List[str] = None) -> pd.DataFrame:
-        if exclude_days:
-            df = df[~df['Day of Week'].isin(exclude_days)]
-        if exclude_stoploss:
-            df = df[~df['Stop Loss %'].isin(exclude_stoploss)]
-        return df
     
+    @staticmethod
+    def filter_days_and_stoploss(df: pd.DataFrame, days: Optional[List[str]] = None, stoploss: Optional[List[str]] = None, include_days: bool = True, include_stoploss: bool = True) -> pd.DataFrame:
+        if days is not None:
+            if include_days:
+                df = df[df['Day of Week'].isin(days)]
+            else:
+                df = df[~df['Day of Week'].isin(days)]
+        
+        if stoploss is not None:
+            if include_stoploss:
+                df = df[df['Stop Loss %'].isin(stoploss)]
+            else:
+                df = df[~df['Stop Loss %'].isin(stoploss)]
+        
+        return df
